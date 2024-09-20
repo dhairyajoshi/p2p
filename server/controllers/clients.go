f70a35885262362ca56b402dfe2bcda9260186d5
Dhairya <joshidhairaya2002@gmail.com>
2024-09-21 02:41:52 +0530

    chore: updating client details

--- server/controllers/clients.go
+++ server/controllers/clients.go
@@ -36,6 +36,20 @@ func RegisterClient(context *gin.Context){
 		return
 	}
 	fmt.Println(client)
+
+	for i, curr_client := range(allClients){
+		if curr_client.ID == client.ID{
+			if curr_client.Username == client.Username && curr_client.Password==client.Password{
+				allClients[i].Address = client.Address
+				allClients[i].PublicKey = client.PublicKey
+				context.JSON(200, gin.H{"msg": "Details updated!"})
+				return
+			} else{
+				context.JSON(401, gin.H{"msg": "Authentication failed!"})
+				return
+			}
+		}
+	}
 	allClients = append(allClients, client)
 	context.JSON(201, gin.H{"msg": "registered client"})
 }
